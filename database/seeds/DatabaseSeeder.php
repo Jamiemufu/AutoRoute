5b41ff7c307d01b962e2ab710084b2c5847cfb5c
Jamie Evans <jamie@jedesign.xyz>
2019-06-17 21:28:49 +0100

    Added seeder to seed 10 random addresses - Please seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\User::create([
+        $faker = \Faker\Factory::create('en_GB');
 
+        \App\User::create([
             'name' => env('ADMIN_NAME', ''),
             'email' => env('ADMIN_EMAIL', ''),
             'email_verified_at' => now(),
             'password' => bcrypt(env('ADMIN_PASSWORD', '')),
             'role' => 'admin',
             'created_at' => now(),
-
         ]);
+
+        // Added seed of random UK address data for ease
+        for ($i = 0; $i < 10; $i++) 
+        {
+            \App\Restaurant::create([
+                'name' => $faker->name,
+                'street' => $faker->streetName,
+                'city' => $faker->city,
+                'postcode' => $faker->postcode
+            ]);
+        }
     }
 }
