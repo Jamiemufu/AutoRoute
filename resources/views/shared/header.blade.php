8763d7b7be905342f3e3628a3ac945dc27bdec57
Jamie Evans <jamieevans@itg.co.uk>
2019-07-02 12:58:40 +0100

    tidy up header file

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -6,16 +6,18 @@
         </div>
 
         <ul>
+            {{-- hide search button on all routes except home --}}
             @if (Request::path() == '/')
-            <li id="search">
-                <form action="" id="searchForm">
-                    <input name="address" type="text" placeholder="Enter your Postcode/Address" id="address">
-                    <button id="searchBtn" type="button">
-                        <i class="fas fa-search"></i>Search
-                    </button>
-                </form>
-            </li>
+                <li id="search">
+                    <form action="" id="searchForm">
+                        <input name="address" type="text" placeholder="Enter your Postcode/Address" id="address">
+                        <button id="searchBtn" type="button">
+                            <i class="fas fa-search"></i>Search
+                        </button>
+                    </form>
+                </li>
             @endif
+
             <a href="/">
                 <li>Home</li>
             </a>
@@ -23,6 +25,7 @@
             <a href="/admin/restaurants">
                 <li>Restaurants</li>
             </a>
+            
             {{-- ony show logout if user authenticated --}}
             @if (Auth::check())
 
